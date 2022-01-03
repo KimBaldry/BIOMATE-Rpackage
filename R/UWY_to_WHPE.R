d3a0f964b670ce27f802cae27752ecfd662dbd50
Kim <baldry.kimberlee@gmail.com>
2022-01-03 18:35:48 +1100

    Update UWY_to_WHPE.R

--- R/UWY_to_WHPE.R
+++ R/UWY_to_WHPE.R
@@ -291,7 +291,6 @@ UWY_to_WHPE = function(file_path, path_out,userID = "IMASUTASKB",row_start = 1,r
 
 
     ### reformat date and time
-    if(!is.empty(info$DATE) & !all(is.na(data2$DATE))){
     if(info$DATE_format == "sec_since_year_start"){
       Y = substr(ex,5,8)
       if(!is.empty(info$DATE)){data2$DATE = as.POSIXct(paste(Y,"01","01", sep = "-"), tz = info$TZ) + as.numeric(data2$DATE)
